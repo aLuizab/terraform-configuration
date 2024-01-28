7c5fe7653f91f64bf9bb0b9faaea01fa952c5d9c
aLuizab <aluiza.primo@gmail.com>
2024-01-28 20:30:11 -0300

    creating a vpc and subnets

--- vars.tf
+++ vars.tf
@@ -1,24 +1,18 @@
 variable "aws_region" {
-  default = "us-east-1"
+  default = "us-east-2"
 }
 
 variable "environment" {
-  default = "javatodev"
+  default = "primbot-develop"
 }
 
 variable "vpc_cidr" {
-  default     = "10.0.0.0/16"
+  default     = "172.0.0.0/16"
   description = "CIDR block of the vpc"
 }
 
 variable "public_subnets_cidr" {
   type        = list(any)
-  default     = ["10.0.0.0/20", "10.0.128.0/20"]
+  default     = ["172.0.1.0/24", "172.0.2.0/24", "172.0.3.0/24"]
   description = "CIDR block for Public Subnet"
-}
-
-variable "private_subnets_cidr" {
-  type        = list(any)
-  default     = ["10.0.16.0/20", "10.0.144.0/20"]
-  description = "CIDR block for Private Subnet"
 }
